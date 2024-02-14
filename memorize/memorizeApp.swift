8375b17a635e2da45b56fbb91498cad0c2a5d4f5
Iiuliia <yulixan29@gmail.com>
2024-02-14 16:54:10 +0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Юлия Прищепкина on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
