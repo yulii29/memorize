44527b2ccffd9f07eabb0bd36651c8fa09ea2734
Iiuliia <yulixan29@gmail.com>
2024-02-15 19:31:16 +0300

    initial commit

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .foregroundColor(Color.purple)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
